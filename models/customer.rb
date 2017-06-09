9c0ec7c4ce19807b4954a8631065e62bf1e25334
Faisal <faisal_a@live.co.uk>
2017-06-09 14:10:01 +0100

    cinema.sql file setup

--- models/customer.rb
+++ models/customer.rb
@@ -8,10 +8,10 @@ class Customer
   def initialize(options)
     @id = options['id'].to_i
     @name = options['name']
-    @funds = options['funds']
+    @funds = options['funds'].to_i
   end
 
-  
+
 
 
 
--- models/ticket.rb
+++ models/ticket.rb
@@ -2,13 +2,15 @@ require_relative('../db/sql_runner.rb')
 
 class Ticket
 
+  attr_accessor :id, :customer_id, :film_id
 
   def initializa(options)
     @id = options['id'].to_i
-    
+    @customer_id = options['customer_id'].to_i
+    @film_id = options['film_id'].to_i
   end
 
-
+  
 
 
 
